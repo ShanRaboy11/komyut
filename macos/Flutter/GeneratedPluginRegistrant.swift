446df3421dd8c6ae71a7dd09be3a5f322c51a2b1
Shanii <shanraboy11@gmail.com>
2025-10-13 15:37:39 +0800

    fix: branch conflicts

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-=======
 import app_links
 import file_picker
 import path_provider_foundation
@@ -17,22 +12,9 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-=======
-import app_links
-import file_picker
-import path_provider_foundation
-import shared_preferences_foundation
-import url_launcher_macos
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> Stashed changes
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
